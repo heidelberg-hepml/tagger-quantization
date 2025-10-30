14286ce506a81c3066ccb5c64a4dd4aa5a248261
Antoine Petitjean <antoine.petitjean1@ens-lyon.fr>
2025-10-30 15:58:39 +0100

    Undo black formatting with wrong version

--- experiments/ranger.py
+++ experiments/ranger.py
@@ -251,10 +251,10 @@ class Lookahead(Optimizer):
                     param_state["cached_params"].copy_(p.data)
                     if self.pullback_momentum == "pullback":
                         internal_momentum = self.optimizer.state[p]["momentum_buffer"]
-                        self.optimizer.state[p]["momentum_buffer"] = (
-                            internal_momentum.mul_(self.alpha).add_(
-                                param_state["cached_mom"], alpha=1.0 - self.alpha
-                            )
+                        self.optimizer.state[p][
+                            "momentum_buffer"
+                        ] = internal_momentum.mul_(self.alpha).add_(
+                            param_state["cached_mom"], alpha=1.0 - self.alpha
                         )
                         param_state["cached_mom"] = self.optimizer.state[p][
                             "momentum_buffer"
