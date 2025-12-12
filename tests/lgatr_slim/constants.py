9e088aad74be70ab265512a7fc12cdf435478b2f
Jonas Spinner <jonasspinner@gmx.de>
2025-12-12 17:03:16 +0100

    Import LGATrSlim from lgatr repo

--- tests/lgatr_slim/constants.py
+++
@@ -1,6 +0,0 @@
-# Default tolerances
-TOLERANCES = dict(atol=1e-3, rtol=1e-4)
-MILD_TOLERANCES = dict(atol=0.05, rtol=0.05)
-STRICT_TOLERANCES = dict(atol=1e-6, rtol=1e-6)
-
-BATCH_DIMS = [[10, 10], [1000]]
